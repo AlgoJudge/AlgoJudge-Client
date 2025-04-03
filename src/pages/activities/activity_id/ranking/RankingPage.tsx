0a2e7f057a047da01808d6c677ab03fd4293dcf5
Adam Zielinski <adam.zie231@gmail.com>
2025-04-03 13:48:16 +0200

    podstawowe wyświetlanie tablicy

--- src/pages/activities/activity_id/ranking/RankingPage.tsx
+++ src/pages/activities/activity_id/ranking/RankingPage.tsx
@@ -2,10 +2,12 @@ import { Title } from "@mantine/core";
 import classes from "./RankingPage.module.css";
 import { useTranslation } from "react-i18next";
 
+import { Table } from "@mantine/core";
+
 interface Contestant {
 	id: string;
 	name: string;
-	completedProblems?: FinishedProblem[];
+	completedProblems: FinishedProblem[];
 }
 
 interface FinishedProblem {
@@ -13,35 +15,35 @@ interface FinishedProblem {
 	points: number;
 }
 
-interface Problem {
+interface Round {
 	id: string;
 	name: string;
 }
 
 interface Model {
 	activityName: string;
-	problems: Problem[];
+	rounds: Round[];
 	contestants: Contestant[];
 }
 
 const data: Model = {
-	activityName: "Konkurs 1",
-	problems: [
+	activityName: "Contest 1",
+	rounds: [
 		{
 			id: "id1",
-			name: "Zadanie 1",
+			name: "Round 1",
 		},
 		{
 			id: "id2",
-			name: "Zadanie 2",
+			name: "Round 2",
 		},
 		{
 			id: "id3",
-			name: "Zadanie 3",
+			name: "Round 3",
 		},
 		{
 			id: "id4",
-			name: "Zadanie 4",
+			name: "Round 4",
 		},
 	],
 	contestants: [
@@ -106,11 +108,50 @@ const data: Model = {
 	],
 };
 
+const rows = data.contestants.map((contestant) => {
+	let completedSum: number = 0;
+	let pointsSum: number = 0;
+	contestant.completedProblems.forEach((problem) => {
+		completedSum++;
+		pointsSum += problem.points;
+	});
+
+	return (
+		<Table.Tr key={contestant.id}>
+			<Table.Td>{contestant.name}</Table.Td>
+			<Table.Td>{`${completedSum}/${data.rounds.length}`}</Table.Td>
+			<Table.Td>{pointsSum}</Table.Td>
+			{data.rounds.map((problem) => (
+				<Table.Td>
+					{
+						contestant.completedProblems.find((user) => user.id === problem.id)
+							?.points
+					}
+				</Table.Td>
+			))}
+		</Table.Tr>
+	);
+});
+
 export default function RankingPage() {
 	const { t } = useTranslation();
 	return (
 		<>
 			<Title>{t("Ranking")}</Title>
+
+			<Table>
+				<Table.Thead>
+					<Table.Tr>
+						<Table.Th>User</Table.Th>
+						<Table.Th>Completed Problems</Table.Th>
+						<Table.Th>Sum</Table.Th>
+						{data.rounds.map((problem) => (
+							<Table.Th>{problem.name}</Table.Th>
+						))}
+					</Table.Tr>
+				</Table.Thead>
+				<Table.Tbody>{rows}</Table.Tbody>
+			</Table>
 		</>
 	);
 }
