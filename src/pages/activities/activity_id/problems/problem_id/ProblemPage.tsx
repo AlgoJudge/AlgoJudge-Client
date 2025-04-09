2f7554b134495ffbcd8240e8fadbc3b8f895f371
Igor Wieprzkowski <igorwieprzkowski@mat.umk.pl>
2025-04-09 23:37:56 +0200

    Zaimplementowano statyczny template strony "Zadanie"

--- src/pages/activities/activity_id/problems/problem_id/ProblemPage.tsx
+++ src/pages/activities/activity_id/problems/problem_id/ProblemPage.tsx
@@ -1,13 +1,19 @@
-import { Button, Group, Title } from "@mantine/core";
+import {Button, Group, Stack, Title} from "@mantine/core";
 import classes from "./ProblemPage.module.css"
 import { useTranslation } from "react-i18next";
 import { Link, useNavigate, useParams } from "react-router-dom";
 
-interface Model {
-    /* ... */
+interface Problem {
+    id: string,
+    name: string,
+    pdf: string
 }
 
-const data: Model = { /* ... */};
+const data: Problem = {
+    id: "P1",
+    name: "Problem 1",
+    pdf: "https://www.mat.umk.pl/panel/wp-content/uploads/matematyka.pdf"
+};
 
 export default function ProblemPage() {
     const { t } = useTranslation();
@@ -15,11 +21,21 @@ export default function ProblemPage() {
     const params = useParams();
     return (
         <>
-            <Title>{t("Problem")}</Title>
-            <Group justify="space-between">
-                <Button onClick={() => navigate(-1)}>{t("Back")}</Button>
-                <Button component={Link} to={`/activities/${params.activityId}/submit/${params.problemId}`}>{t("Submit")}</Button>
-            </Group>
+            <Stack spacing={2}>
+                <Title>{data.name}</Title>
+                <Group justify="space-between">
+                    <Button onClick={() => navigate(-1)}>{t("Back")}</Button>
+                    <Group>
+                        <Button component={Link} to={data.pdf} download target="_self">{t("Download task")}</Button>
+                        <Button component={Link} to={`/activities/${params.activityId}/submit/${params.problemId}`}>{t("Submit")}</Button>
+                    </Group>
+                </Group>
+                <Group h={600}>
+                    <object data={data.pdf} type="application/pdf" width="100%" height="100%">
+                        <p>PDF should be here</p>
+                    </object>
+                </Group>
+            </Stack>
         </>
     );
 }
